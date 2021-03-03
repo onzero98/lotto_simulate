80823cb247bc3cd4c1d7c421746f5e023fe175de
KIMIH98 <ryueun0404@naver.com>
2021-03-03 16:40:59 +0900

    Add files via upload
    load codes

---
+++ lotto.py
@@ -0,0 +1,46 @@
+from random import randint
+
+def generate_numbers(n):
+    numbers = []
+
+    while len(numbers) < n:
+        num = randint(1, 45)
+        
+        if num not in numbers:
+            numbers.append(num)
+	
+    numbers.sort()
+    return numbers
+
+def gacha():
+    gacha = generate_numbers(7)
+
+    # 6개의 일반 당첨 번호 + 1개의 보너스 번호
+    result = sorted(gacha[:6]) + gacha[6:]
+    return result
+
+def count_matches(numbers, win_numbers):
+    count = 0
+
+    for num in numbers:
+        if num in win_numbers:
+            count += 1
+
+    return count
+
+def check_prize(numbers, win_numbers):
+    count = count_matches(numbers, win_numbers[:6])
+    bonus = count_matches(numbers, win_numbers[6:])
+
+    if count == 6:
+        return 1000000000
+    elif count == 5 and bonus == 1:
+        return 50000000
+    elif count == 5:
+        return 1000000
+    elif count == 4:
+        return 50000
+    elif count == 3:
+        return 5000
+    else:
+        return 0
